be4c7f5d7520298878f14fa06435e7321a4f1f19
PANDETI SAI VINITHA <113926586+P-Sai-Vinitha@users.noreply.github.com>
2023-09-02 08:42:47 +0530

    Add files via upload

---
+++ ab.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class ab{
+public static void main(String args[])
+{
+int n;
+Scanner scanner=new Scanner(System.in);
+n=scanner.nextInt();
+System.identityHashCode(n);
+}
+}
